ccdcd9d3e5359dd62ab9f76bea36c9d2d639adc6
rakiraki829613-a11y <rakiraki829613@gmail.com>
2026-03-25 22:21:15 +0530

    if-else program

---
+++ day2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=56;
+    if(n>=0){
+        cout<<"n is nagative";
+    }
+        else {
+        cout<<" n is positive";
+    }
+    return 0;
+}
