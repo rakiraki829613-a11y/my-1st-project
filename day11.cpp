2538d2ef30b31b55e4a20b55332d44b073156578
rakiraki829613-a11y <rakiraki829613@gmail.com>
2026-04-01 23:24:08 +0530

    dfghjk

--- day11.cpp
+++ day11.cpp
@@ -1,17 +1,9 @@
 #include<iostream>
 using namespace std;
 
-int binarynum(int decinum){
-    int ans=0; int pow=1;
-    while(decinum>0){
-    int rem=decinum%10;
-    ans=ans+(rem*pow);
-    decinum=decinum/10;
-    pow=pow*2;
-    }
-    return ans;
-}
 int main(){
-        cout<<binarynum(101)<<endl;
-        return 0;
+    int marks[5]={99,80,90,89,78};
+    int size=5;
+    cout<<sizeof(marks)<<endl;
+    return 0;
 }
